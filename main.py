2b99ec7085cb89df0f6c835199148b7af12eec98
Tylor Shin <shincode@icloud.com>
2018-07-15 10:38:55 +0900

    init

---
+++ main.py
@@ -0,0 +1,74 @@
+import sqlite3
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import datetime
+
+seed_date = "2018-07-24"
+date_range = 3
+search_range_in_day = 61
+start_airport = "ICN"
+end_airport = "CTS"
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36'}
+conn = sqlite3.connect("db/test.db")
+cur = conn.cursor()
+cur.execute(
+    "CREATE TABLE IF NOT EXISTS trips (id integer PRIMARY KEY, start_airport text, end_airport text, price integer, start_date text, end_date text, created_at DATETIME DEFAULT CURRENT_TIMESTAMP)")
+options = webdriver.ChromeOptions()
+options.add_argument('headless')
+options.add_argument('window-size=1920x1080')
+options.add_argument("disable-gpu")
+driver = webdriver.Chrome(executable_path="chromedriver.exe", chrome_options=options)
+driver.set_page_load_timeout(300)
+driver.implicitly_wait(3)
+
+
+def search_min_price(d1, d2):
+    path = "https://store.naver.com/flights/v2/results?trip=RT&scity1=%s&ecity1=%s&scity2=%s&ecity2=%s&adult=3&child=0&infant=0&sdate1=%s.&sdate2=%s.&fareType=Y" % (
+        start_airport, end_airport, end_airport, start_airport, d1, d2)
+    driver.get(path)
+    try:
+        WebDriverWait(driver, 10).until(
+            EC.visibility_of_any_elements_located((By.CLASS_NAME, "tit_apply"))
+        )
+        html = driver.page_source
+        soup = BeautifulSoup(html, "html.parser")
+        prices = soup.find_all("span", class_="txt_pay")
+        driver.get_screenshot_as_file("./dst/success.png")
+        price = prices[0]
+        price_str = price.get_text(strip="true")
+        p = price_str.replace(",", "")
+        print(p)
+        sql = "INSERT OR IGNORE INTO trips(start_airport, end_airport, price, start_date, end_date) values (?, ?, ?, ?, ?)"
+        cur.execute(sql, (start_airport, end_airport, int(p), d1, d2))
+        conn.commit()
+    except Exception as e:
+        print(e)
+        driver.get_screenshot_as_file("./dst/fail.png")
+
+d1 = datetime.date.fromisoformat(seed_date)
+d2 = d1 + datetime.timedelta(days=date_range)
+d1_string = d1.strftime("%Y.%m.%d")
+d2_string = d2.strftime("%Y.%m.%d")
+
+try:
+    for i in range(0, search_range_in_day):
+        seed_d1 = datetime.date.fromisoformat(seed_date)
+        d1 = seed_d1 + datetime.timedelta(days=i)
+        d2 = d1 + datetime.timedelta(days=date_range)
+        d1_string = d1.strftime("%Y.%m.%d")
+        d2_string = d2.strftime("%Y.%m.%d")
+        search_min_price(d1_string, d2_string)
+except Exception as e:
+    print(e)
+finally:
+    cur.execute("SELECT * FROM trips")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    driver.quit()
+    conn.close()
